4a72e2a2d88edbf9742a9a86771b6470c231e70d
Collasperz <120536920+Collasperz@users.noreply.github.com>
2023-11-10 18:50:07 -0600

    hi

--- __settings__.js
+++ __settings__.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "https://cdn.jsdelivr.net/gh/collasperz/ran-obby/1856469.json";
+window.SCENE_PATH = "1856469.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
@@ -9,7 +9,7 @@ window.CONTEXT_OPTIONS = {
     'powerPreference': "high-performance"
 };
 window.SCRIPTS = [ 151975456, 151975458, 151975466, 151982579, 151982883, 151992958, 151992959, 151992965, 151992968, 151992974, 151992989, 151992990, 151992992, 151992993, 152048458, 152048495, 152048512, 152048513, 152048931, 152048515, 152048688, 152048709, 152048723, 152048867, 152048913, 152048922, 152048921, 152048928, 152048942, 152048946, 152048962, 152048964, 152048965, 152048966, 152048967, 152048968, 152049683, 152067136, 152069093, 152131291, 152131292, 152131296, 152131300, 152131302, 152131305, 152131306, 152131307, 152131312, 152131316, 152131317, 152131309, 152131310, 152131313, 152131319, 152131320, 152131321, 152131322, 152131324, 152131318, 152131409, 152048863, 152360243, 152449480, 152542414, 152570757, 152589849, 152131299, 152048475, 152059040, 151992952, 152040705, 151992988, 151982950, 151992967 ];
-window.CONFIG_FILENAME = "https://cdn.jsdelivr.net/gh/collasperz/ran-obby/config.json";
+window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
@@ -18,5 +18,5 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
-    {'moduleName' : 'Ammo', 'glueUrl' : 'https://cdn.jsdelivr.net/gh/collasperz/ran-obby/files/assets/152049618/1/ammo.wasm.js', 'wasmUrl' : 'https://cdn.jsdelivr.net/gh/collasperz/ran-obby/files/assets/152049619/1/ammo.wasm.wasm', 'fallbackUrl' : 'https://cdn.jsdelivr.net/gh/collasperz/ran-obby/files/assets/152049617/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/152049618/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/152049619/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/152049617/1/ammo.js', 'preload' : true},
 ];
